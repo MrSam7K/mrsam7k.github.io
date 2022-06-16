1b663f20eca1cbe281755065080d13599214d0dd
MrSam7K <mrsam7k@gmail.com>
2022-06-16 17:56:17 +0300

    Countdown stuff

---
+++ script.js
@@ -0,0 +1,22 @@
+const countdown = () => {
+    const countDate = new Date('June 18, 2022 14:00:00').getTime()
+    const currentDate = new Date().getTime()
+    const gap = countDate - currentDate
+
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const textDay = Math.floor(gap / day)
+    const textHour = Math.floor((gap % day) / hour)
+    const textMin = Math.floor((gap % hour) / minute)
+    const textSec = Math.floor((gap % minute) / second)
+
+    document.querySelector(".day").innerText = textDay
+    document.querySelector(".hour").innerText = textHour
+    document.querySelector(".min").innerText = textMin
+    document.querySelector(".sec").innerText = textSec
+}
+
+setInterval(countdown, 1000)
