eb84c901a1f48f5f8eac490a3926a7e09d6e33b3
MrSam7K <mrsam7k@gmail.com>
2022-06-16 18:28:38 +0300

    Last things before finishing up!

--- script.js
+++ script.js
@@ -13,35 +13,43 @@ const countdown = () => {
     const textMin = Math.floor((gap % hour) / minute)
     const textSec = Math.floor((gap % minute) / second)
 
-    document.querySelector(".day").innerText = textDay
-    document.querySelector(".hour").innerText = textHour
-    document.querySelector(".min").innerText = textMin
-    document.querySelector(".sec").innerText = textSec
+    document.querySelector(".day").innerText = textDay + 'd'
+    document.querySelector(".hour").innerText = textHour + 'h'
+    document.querySelector(".min").innerText = textMin + 'm'
+    document.querySelector(".sec").innerText = textSec + 's'
 
     if(textDay <= 0){
         document.querySelector(".day").innerText = ""
         document.querySelector(".dayText").innerText = ""
     } else {
-        document.querySelector(".dayText").innerText = "Dienos"
+        document.querySelector(".dayText").innerText = "AAAAA"
     }
     if(textHour <= 0){
         document.querySelector(".hour").innerText = ""
         document.querySelector(".hourText").innerText = ""
     } else {
-        document.querySelector(".hourText").innerText = "Valandos"
+        document.querySelector(".hourText").innerText = "AAAAAAAAA"
     }
     if(textMin <= 0){
         document.querySelector(".min").innerText = ""
         document.querySelector(".minText").innerText = ""
     } else {
-        document.querySelector(".minText").innerText = "Minutės"
+        document.querySelector(".minText").innerText = "AAAAAAA"
     }
     if(textSec <= 0){
         document.querySelector(".sec").innerText = ""
         document.querySelector(".secText").innerText = ""
     } else {
-        document.querySelector(".secText").innerText = "Sekundės"
+        document.querySelector(".secText").innerText = "AAAAAAAA"
     }
+
+    if(gap <= 0){
+        document.querySelector(".day").innerText = ""
+    document.querySelector(".hour").innerText = "Sveikas s"
+    document.querySelector(".min").innerText = "ugrįžęs!"
+    document.querySelector(".sec").innerText = ""
+    }
+    console.log(gap)
 }
 
-setInterval(countdown, 500)
+setInterval(countdown, 1000)
